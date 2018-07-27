1d9f9a16f7dff1840e8ad69306bc993439886b35
Jason Laster <jason.laster.11@gmail.com>
2018-07-27 16:40:56 -0400

    organize content

--- src/components/feature.js
+++ src/components/feature.js
@@ -2,18 +2,12 @@ import React from 'react'
 import styles from './feature.module.css'
 
 export default props => {
-  const excerpt = Array.isArray(props.excerpt) ? (
-    props.excerpt.map(e => <p>{e}</p>)
-  ) : (
-    <p>{props.excerpt}</p>
-  )
-  console.log(excerpt)
   return (
     <div className={styles.feature}>
       <img src={props.img} className={styles.img} alt="" />
       <div className={styles.description}>
         <h2 className={styles.name}>{props.name}</h2>
-        <div className={styles.excerpt}>{excerpt}</div>
+        <div className={styles.excerpt}>{props.children}</div>
       </div>
     </div>
   )
